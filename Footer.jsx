8d4d3600941713450130fdb65ec269d20d502eaa
harry413 <73785500+harry413@users.noreply.github.com>
2021-06-05 12:46:39 +0530

    Add files via upload

---
+++ Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Footer() {
+  var curryear = new Date().getFullYear();
+
+  return (
+    <footer>
+      <p>copyright @ {curryear}</p>
+    </footer>
+  );
+}
+export default Footer;
---
+++ Note.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1> Web-dev with javascript and react.js</h1>
+      <p>
+        <strong>The</strong> bootcamp for web-devlopment with javascript and
+        react.js . this was an amazing bootcamp we covered everything from
+        scratch including javascript react.js and html. i personally love this
+        bootcamp|
+        <br />
+        <br />
+        <em>
+          <u>
+            thank you so much <strong>shaurya sinha</strong> sir
+          </u>
+        </em>
+      </p>
+    </div>
+  );
+}
+
+export default Note;
